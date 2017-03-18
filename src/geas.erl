118f734f474796fd69a4dac210594877b8d129dc
crownedgrouse <eric.pailleau@wanadoo.fr>
2017-03-18 13:58:36 +0100

    Erlang 19.3 update

--- src/geas.erl
+++ src/geas.erl
@@ -790,6 +790,7 @@ get_author(File) ->
 %%-------------------------------------------------------------------------
 -spec get_erlang_version(list()) -> {list(), list(), list()} | undefined.
 
+get_erlang_version("7.0.4")     -> {"19.3", "19.3", "19.3"};
 get_erlang_version("7.0.3")     -> {"19.2", "19.2", "19.2"};
 get_erlang_version("7.0.2")     -> {"19.1", "19.1", "19.1"};
 get_erlang_version("7.0")       -> {"19.0", "19.0", "19.0"};
