a0b531d32cc082d4b2134328f80cc126478167b1
crownedgrouse <eric.pailleau@wanadoo.fr>
2023-12-23 16:14:44 +0100

    Update database and detection for OTP-26.2

--- src/geas_db.erl
+++ src/geas_db.erl
@@ -40,7 +40,7 @@
                    "23.0", "23.1", "23.2", "23.3",
                    "24.0", "24.1", "24.2", "24.3",
 		   "25.0", "25.1", "25.2", "25.3",
-		   "26.0", "26.1"]).
+		   "26.0", "26.1", "26.2"]).
 
 %% This module generate the geas_db.hrl
 %% providing the min and max release of any Erlang/OTP function
